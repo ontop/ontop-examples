c24247935cf24709e43d8c42630b611c92210870
tir <davide.lanti@unibz.it>
2015-03-05 14:05:20 +0100

    NPD Data instances

---
+++ vldb-2015/CTE Experiments/NPD/db2/createDatabaseScripts/db2dropfkeys.sql
@@ -0,0 +1,485 @@
+-- This file was generated on : 11-04-2014 13:21:15
+-- IBM Data Movement Tool Version : 2.00-b2126
+--
+-- This script will drop FOREIGN KEY constraints and is part of the db2dropobjects.sh script.
+-- 
+--#SET :DROP FOREIGN KEY::FK1_baaArea
+ALTER TABLE "npd_no_spatial_10"."baaArea"
+DROP CONSTRAINT FK1_baaArea
+;
+
+--#SET :DROP FOREIGN KEY::FK2_bsns_arr_area_area_poly_hst
+ALTER TABLE "npd_no_spatial_10"."bsns_arr_area_area_poly_hst"
+DROP CONSTRAINT FK2_bsns_arr_area_area_poly_hst
+;
+
+--#SET :DROP FOREIGN KEY::FK3_bsns_arr_area_transfer_hst
+ALTER TABLE "npd_no_spatial_10"."bsns_arr_area_transfer_hst"
+DROP CONSTRAINT FK3_bsns_arr_area_transfer_hst
+;
+
+--#SET :DROP FOREIGN KEY::FK4_bsns_arr_area_transfer_hst
+ALTER TABLE "npd_no_spatial_10"."bsns_arr_area_transfer_hst"
+DROP CONSTRAINT FK4_bsns_arr_area_transfer_hst
+;
+
+--#SET :DROP FOREIGN KEY::FK5_company_reserves
+ALTER TABLE "npd_no_spatial_10"."company_reserves"
+DROP CONSTRAINT FK5_company_reserves
+;
+
+--#SET :DROP FOREIGN KEY::FK5_dscArea
+ALTER TABLE "npd_no_spatial_10"."dscArea"
+DROP CONSTRAINT FK5_dscArea
+;
+
+--#SET :DROP FOREIGN KEY::FK6_company_reserves
+ALTER TABLE "npd_no_spatial_10"."company_reserves"
+DROP CONSTRAINT FK6_company_reserves
+;
+
+--#SET :DROP FOREIGN KEY::FK7_dscArea
+ALTER TABLE "npd_no_spatial_10"."dscArea"
+DROP CONSTRAINT FK7_dscArea
+;
+
+--#SET :DROP FOREIGN KEY::FK9_dscArea
+ALTER TABLE "npd_no_spatial_10"."dscArea"
+DROP CONSTRAINT FK9_dscArea
+;
+
+--#SET :DROP FOREIGN KEY::FK10_bsns_arr_area_operator
+ALTER TABLE "npd_no_spatial_10"."bsns_arr_area_operator"
+DROP CONSTRAINT FK10_bsns_arr_area_operator
+;
+
+--#SET :DROP FOREIGN KEY::FK11_bsns_arr_area_operator
+ALTER TABLE "npd_no_spatial_10"."bsns_arr_area_operator"
+DROP CONSTRAINT FK11_bsns_arr_area_operator
+;
+
+--#SET :DROP FOREIGN KEY::FK11_bsns_arr_area_licensee_hst
+ALTER TABLE "npd_no_spatial_10"."bsns_arr_area_licensee_hst"
+DROP CONSTRAINT FK11_bsns_arr_area_licensee_hst
+;
+
+--#SET :DROP FOREIGN KEY::FK13_bsns_arr_area_licensee_hst
+ALTER TABLE "npd_no_spatial_10"."bsns_arr_area_licensee_hst"
+DROP CONSTRAINT FK13_bsns_arr_area_licensee_hst
+;
+
+--#SET :DROP FOREIGN KEY::FK14_discovery_reserves
+ALTER TABLE "npd_no_spatial_10"."discovery_reserves"
+DROP CONSTRAINT FK14_discovery_reserves
+;
+
+--#SET :DROP FOREIGN KEY::FK15_discovery
+ALTER TABLE "npd_no_spatial_10"."discovery"
+DROP CONSTRAINT FK15_discovery
+;
+
+--#SET :DROP FOREIGN KEY::FK16_discovery
+ALTER TABLE "npd_no_spatial_10"."discovery"
+DROP CONSTRAINT FK16_discovery
+;
+
+--#SET :DROP FOREIGN KEY::FK17_facility_moveable
+ALTER TABLE "npd_no_spatial_10"."facility_moveable"
+DROP CONSTRAINT FK17_facility_moveable
+;
+
+--#SET :DROP FOREIGN KEY::FK18_field_activity_status_hst
+ALTER TABLE "npd_no_spatial_10"."field_activity_status_hst"
+DROP CONSTRAINT FK18_field_activity_status_hst
+;
+
+--#SET :DROP FOREIGN KEY::FK19_fclPoint
+ALTER TABLE "npd_no_spatial_10"."fclPoint"
+DROP CONSTRAINT FK19_fclPoint
+;
+
+--#SET :DROP FOREIGN KEY::FK20_fclPoint
+ALTER TABLE "npd_no_spatial_10"."fclPoint"
+DROP CONSTRAINT FK20_fclPoint
+;
+
+--#SET :DROP FOREIGN KEY::FK21_field
+ALTER TABLE "npd_no_spatial_10"."field"
+DROP CONSTRAINT FK21_field
+;
+
+--#SET :DROP FOREIGN KEY::FK22_field
+ALTER TABLE "npd_no_spatial_10"."field"
+DROP CONSTRAINT FK22_field
+;
+
+--#SET :DROP FOREIGN KEY::FK23_field
+ALTER TABLE "npd_no_spatial_10"."field"
+DROP CONSTRAINT FK23_field
+;
+
+--#SET :DROP FOREIGN KEY::FK23_field_description
+ALTER TABLE "npd_no_spatial_10"."field_description"
+DROP CONSTRAINT FK23_field_description
+;
+
+--#SET :DROP FOREIGN KEY::FK25_field_investment_yearly
+ALTER TABLE "npd_no_spatial_10"."field_investment_yearly"
+DROP CONSTRAINT FK25_field_investment_yearly
+;
+
+--#SET :DROP FOREIGN KEY::FK26_field_licensee_hst
+ALTER TABLE "npd_no_spatial_10"."field_licensee_hst"
+DROP CONSTRAINT FK26_field_licensee_hst
+;
+
+--#SET :DROP FOREIGN KEY::FK27_field_licensee_hst
+ALTER TABLE "npd_no_spatial_10"."field_licensee_hst"
+DROP CONSTRAINT FK27_field_licensee_hst
+;
+
+--#SET :DROP FOREIGN KEY::FK28_field_operator_hst
+ALTER TABLE "npd_no_spatial_10"."field_operator_hst"
+DROP CONSTRAINT FK28_field_operator_hst
+;
+
+--#SET :DROP FOREIGN KEY::FK29_field_operator_hst
+ALTER TABLE "npd_no_spatial_10"."field_operator_hst"
+DROP CONSTRAINT FK29_field_operator_hst
+;
+
+--#SET :DROP FOREIGN KEY::FK30_field_owner_hst
+ALTER TABLE "npd_no_spatial_10"."field_owner_hst"
+DROP CONSTRAINT FK30_field_owner_hst
+;
+
+--#SET :DROP FOREIGN KEY::FK31_fldArea
+ALTER TABLE "npd_no_spatial_10"."fldArea"
+DROP CONSTRAINT FK31_fldArea
+;
+
+--#SET :DROP FOREIGN KEY::FK32_fldArea
+ALTER TABLE "npd_no_spatial_10"."fldArea"
+DROP CONSTRAINT FK32_fldArea
+;
+
+--#SET :DROP FOREIGN KEY::FK33_fldArea
+ALTER TABLE "npd_no_spatial_10"."fldArea"
+DROP CONSTRAINT FK33_fldArea
+;
+
+--#SET :DROP FOREIGN KEY::FK34_licence_licensee_hst
+ALTER TABLE "npd_no_spatial_10"."licence_licensee_hst"
+DROP CONSTRAINT FK34_licence_licensee_hst
+;
+
+--#SET :DROP FOREIGN KEY::FK35_licence_licensee_hst
+ALTER TABLE "npd_no_spatial_10"."licence_licensee_hst"
+DROP CONSTRAINT FK35_licence_licensee_hst
+;
+
+--#SET :DROP FOREIGN KEY::FK36_licence_area_poly_hst
+ALTER TABLE "npd_no_spatial_10"."licence_area_poly_hst"
+DROP CONSTRAINT FK36_licence_area_poly_hst
+;
+
+--#SET :DROP FOREIGN KEY::FK37_field_reserves
+ALTER TABLE "npd_no_spatial_10"."field_reserves"
+DROP CONSTRAINT FK37_field_reserves
+;
+
+--#SET :DROP FOREIGN KEY::FK38_licence_petreg_licence
+ALTER TABLE "npd_no_spatial_10"."licence_petreg_licence"
+DROP CONSTRAINT FK38_licence_petreg_licence
+;
+
+--#SET :DROP FOREIGN KEY::FK39_licence_petreg_licence_oper
+ALTER TABLE "npd_no_spatial_10"."licence_petreg_licence_oper"
+DROP CONSTRAINT FK39_licence_petreg_licence_oper
+;
+
+--#SET :DROP FOREIGN KEY::FK39_licence_petreg_licence_licencee
+ALTER TABLE "npd_no_spatial_10"."licence_petreg_licence_licencee"
+DROP CONSTRAINT FK39_licence_petreg_licence_licencee
+;
+
+--#SET :DROP FOREIGN KEY::FK40_licence_petreg_licence_oper
+ALTER TABLE "npd_no_spatial_10"."licence_petreg_licence_oper"
+DROP CONSTRAINT FK40_licence_petreg_licence_oper
+;
+
+--#SET :DROP FOREIGN KEY::FK41_licence_petreg_licence_licencee
+ALTER TABLE "npd_no_spatial_10"."licence_petreg_licence_licencee"
+DROP CONSTRAINT FK41_licence_petreg_licence_licencee
+;
+
+--#SET :DROP FOREIGN KEY::FK43_licence_petreg_message
+ALTER TABLE "npd_no_spatial_10"."licence_petreg_message"
+DROP CONSTRAINT FK43_licence_petreg_message
+;
+
+--#SET :DROP FOREIGN KEY::FK44_pipLine
+ALTER TABLE "npd_no_spatial_10"."pipLine"
+DROP CONSTRAINT FK44_pipLine
+;
+
+--#SET :DROP FOREIGN KEY::FK45_prlArea
+ALTER TABLE "npd_no_spatial_10"."prlArea"
+DROP CONSTRAINT FK45_prlArea
+;
+
+--#SET :DROP FOREIGN KEY::FK46_prlArea
+ALTER TABLE "npd_no_spatial_10"."prlArea"
+DROP CONSTRAINT FK46_prlArea
+;
+
+--#SET :DROP FOREIGN KEY::FK47_licence_transfer_hst
+ALTER TABLE "npd_no_spatial_10"."licence_transfer_hst"
+DROP CONSTRAINT FK47_licence_transfer_hst
+;
+
+--#SET :DROP FOREIGN KEY::FK48_licence_transfer_hst
+ALTER TABLE "npd_no_spatial_10"."licence_transfer_hst"
+DROP CONSTRAINT FK48_licence_transfer_hst
+;
+
+--#SET :DROP FOREIGN KEY::FK49_licence_task
+ALTER TABLE "npd_no_spatial_10"."licence_task"
+DROP CONSTRAINT FK49_licence_task
+;
+
+--#SET :DROP FOREIGN KEY::FK49_seis_acquisition
+ALTER TABLE "npd_no_spatial_10"."seis_acquisition"
+DROP CONSTRAINT FK49_seis_acquisition
+;
+
+--#SET :DROP FOREIGN KEY::FK50_licence_task
+ALTER TABLE "npd_no_spatial_10"."licence_task"
+DROP CONSTRAINT FK50_licence_task
+;
+
+--#SET :DROP FOREIGN KEY::FK52_licence_task
+ALTER TABLE "npd_no_spatial_10"."licence_task"
+DROP CONSTRAINT FK52_licence_task
+;
+
+--#SET :DROP FOREIGN KEY::FK53_licence_oper_hst
+ALTER TABLE "npd_no_spatial_10"."licence_oper_hst"
+DROP CONSTRAINT FK53_licence_oper_hst
+;
+
+--#SET :DROP FOREIGN KEY::FK54_licence_oper_hst
+ALTER TABLE "npd_no_spatial_10"."licence_oper_hst"
+DROP CONSTRAINT FK54_licence_oper_hst
+;
+
+--#SET :DROP FOREIGN KEY::FK55_seaArea
+ALTER TABLE "npd_no_spatial_10"."seaArea"
+DROP CONSTRAINT FK55_seaArea
+;
+
+--#SET :DROP FOREIGN KEY::FK55_tuf_operator_hst
+ALTER TABLE "npd_no_spatial_10"."tuf_operator_hst"
+DROP CONSTRAINT FK55_tuf_operator_hst
+;
+
+--#SET :DROP FOREIGN KEY::FK57_tuf_operator_hst
+ALTER TABLE "npd_no_spatial_10"."tuf_operator_hst"
+DROP CONSTRAINT FK57_tuf_operator_hst
+;
+
+--#SET :DROP FOREIGN KEY::FK58_seaMultiline
+ALTER TABLE "npd_no_spatial_10"."seaMultiline"
+DROP CONSTRAINT FK58_seaMultiline
+;
+
+--#SET :DROP FOREIGN KEY::FK59_seis_acquisition_coordinates_inc_turnarea
+ALTER TABLE "npd_no_spatial_10"."seis_acquisition_coordinates_inc_turnarea"
+DROP CONSTRAINT FK59_seis_acquisition_coordinates_inc_turnarea
+;
+
+--#SET :DROP FOREIGN KEY::FK60_licence_phase_hst
+ALTER TABLE "npd_no_spatial_10"."licence_phase_hst"
+DROP CONSTRAINT FK60_licence_phase_hst
+;
+
+--#SET :DROP FOREIGN KEY::FK61_tuf_owner_hst
+ALTER TABLE "npd_no_spatial_10"."tuf_owner_hst"
+DROP CONSTRAINT FK61_tuf_owner_hst
+;
+
+--#SET :DROP FOREIGN KEY::FK62_tuf_owner_hst
+ALTER TABLE "npd_no_spatial_10"."tuf_owner_hst"
+DROP CONSTRAINT FK62_tuf_owner_hst
+;
+
+--#SET :DROP FOREIGN KEY::FK63_tuf_petreg_message
+ALTER TABLE "npd_no_spatial_10"."tuf_petreg_message"
+DROP CONSTRAINT FK63_tuf_petreg_message
+;
+
+--#SET :DROP FOREIGN KEY::FK64_prlAreaSplitByBlock
+ALTER TABLE "npd_no_spatial_10"."prlAreaSplitByBlock"
+DROP CONSTRAINT FK64_prlAreaSplitByBlock
+;
+
+--#SET :DROP FOREIGN KEY::FK65_prlAreaSplitByBlock
+ALTER TABLE "npd_no_spatial_10"."prlAreaSplitByBlock"
+DROP CONSTRAINT FK65_prlAreaSplitByBlock
+;
+
+--#SET :DROP FOREIGN KEY::FK66_wellbore_casing_and_lot
+ALTER TABLE "npd_no_spatial_10"."wellbore_casing_and_lot"
+DROP CONSTRAINT FK66_wellbore_casing_and_lot
+;
+
+--#SET :DROP FOREIGN KEY::FK67_strat_litho_wellbore
+ALTER TABLE "npd_no_spatial_10"."strat_litho_wellbore"
+DROP CONSTRAINT FK67_strat_litho_wellbore
+;
+
+--#SET :DROP FOREIGN KEY::FK68_strat_litho_wellbore_core
+ALTER TABLE "npd_no_spatial_10"."strat_litho_wellbore_core"
+DROP CONSTRAINT FK68_strat_litho_wellbore_core
+;
+
+--#SET :DROP FOREIGN KEY::FK69_tuf_petreg_licence_oper
+ALTER TABLE "npd_no_spatial_10"."tuf_petreg_licence_oper"
+DROP CONSTRAINT FK69_tuf_petreg_licence_oper
+;
+
+--#SET :DROP FOREIGN KEY::FK70_tuf_petreg_licence_oper
+ALTER TABLE "npd_no_spatial_10"."tuf_petreg_licence_oper"
+DROP CONSTRAINT FK70_tuf_petreg_licence_oper
+;
+
+--#SET :DROP FOREIGN KEY::FK71_seis_acquisition_progress
+ALTER TABLE "npd_no_spatial_10"."seis_acquisition_progress"
+DROP CONSTRAINT FK71_seis_acquisition_progress
+;
+
+--#SET :DROP FOREIGN KEY::FK72_wellbore_document
+ALTER TABLE "npd_no_spatial_10"."wellbore_document"
+DROP CONSTRAINT FK72_wellbore_document
+;
+
+--#SET :DROP FOREIGN KEY::FK73_tuf_petreg_licence_licencee
+ALTER TABLE "npd_no_spatial_10"."tuf_petreg_licence_licencee"
+DROP CONSTRAINT FK73_tuf_petreg_licence_licencee
+;
+
+--#SET :DROP FOREIGN KEY::FK74_tuf_petreg_licence_licencee
+ALTER TABLE "npd_no_spatial_10"."tuf_petreg_licence_licencee"
+DROP CONSTRAINT FK74_tuf_petreg_licence_licencee
+;
+
+--#SET :DROP FOREIGN KEY::FK75_wellbore_development_all
+ALTER TABLE "npd_no_spatial_10"."wellbore_development_all"
+DROP CONSTRAINT FK75_wellbore_development_all
+;
+
+--#SET :DROP FOREIGN KEY::FK76_wellbore_development_all
+ALTER TABLE "npd_no_spatial_10"."wellbore_development_all"
+DROP CONSTRAINT FK76_wellbore_development_all
+;
+
+--#SET :DROP FOREIGN KEY::FK77_wellbore_development_all
+ALTER TABLE "npd_no_spatial_10"."wellbore_development_all"
+DROP CONSTRAINT FK77_wellbore_development_all
+;
+
+--#SET :DROP FOREIGN KEY::FK78_wellbore_development_all
+ALTER TABLE "npd_no_spatial_10"."wellbore_development_all"
+DROP CONSTRAINT FK78_wellbore_development_all
+;
+
+--#SET :DROP FOREIGN KEY::FK79_wellbore_development_all
+ALTER TABLE "npd_no_spatial_10"."wellbore_development_all"
+DROP CONSTRAINT FK79_wellbore_development_all
+;
+
+--#SET :DROP FOREIGN KEY::FK80_wellbore_development_all
+ALTER TABLE "npd_no_spatial_10"."wellbore_development_all"
+DROP CONSTRAINT FK80_wellbore_development_all
+;
+
+--#SET :DROP FOREIGN KEY::FK81_wellbore_development_all
+ALTER TABLE "npd_no_spatial_10"."wellbore_development_all"
+DROP CONSTRAINT FK81_wellbore_development_all
+;
+
+--#SET :DROP FOREIGN KEY::FK82_wellbore_core_photo
+ALTER TABLE "npd_no_spatial_10"."wellbore_core_photo"
+DROP CONSTRAINT FK82_wellbore_core_photo
+;
+
+--#SET :DROP FOREIGN KEY::FK83_wellbore_core
+ALTER TABLE "npd_no_spatial_10"."wellbore_core"
+DROP CONSTRAINT FK83_wellbore_core
+;
+
+--#SET :DROP FOREIGN KEY::FK83_wellbore_formation_top
+ALTER TABLE "npd_no_spatial_10"."wellbore_formation_top"
+DROP CONSTRAINT FK83_wellbore_formation_top
+;
+
+--#SET :DROP FOREIGN KEY::FK85_wellbore_coordinates
+ALTER TABLE "npd_no_spatial_10"."wellbore_coordinates"
+DROP CONSTRAINT FK85_wellbore_coordinates
+;
+
+--#SET :DROP FOREIGN KEY::FK86_wellbore_dst
+ALTER TABLE "npd_no_spatial_10"."wellbore_dst"
+DROP CONSTRAINT FK86_wellbore_dst
+;
+
+--#SET :DROP FOREIGN KEY::FK87_wlbPoint
+ALTER TABLE "npd_no_spatial_10"."wlbPoint"
+DROP CONSTRAINT FK87_wlbPoint
+;
+
+--#SET :DROP FOREIGN KEY::FK88_wellbore_mud
+ALTER TABLE "npd_no_spatial_10"."wellbore_mud"
+DROP CONSTRAINT FK88_wellbore_mud
+;
+
+--#SET :DROP FOREIGN KEY::FK89_wellbore_exploration_all
+ALTER TABLE "npd_no_spatial_10"."wellbore_exploration_all"
+DROP CONSTRAINT FK89_wellbore_exploration_all
+;
+
+--#SET :DROP FOREIGN KEY::FK90_wellbore_exploration_all
+ALTER TABLE "npd_no_spatial_10"."wellbore_exploration_all"
+DROP CONSTRAINT FK90_wellbore_exploration_all
+;
+
+--#SET :DROP FOREIGN KEY::FK91_wellbore_exploration_all
+ALTER TABLE "npd_no_spatial_10"."wellbore_exploration_all"
+DROP CONSTRAINT FK91_wellbore_exploration_all
+;
+
+--#SET :DROP FOREIGN KEY::FK92_wellbore_exploration_all
+ALTER TABLE "npd_no_spatial_10"."wellbore_exploration_all"
+DROP CONSTRAINT FK92_wellbore_exploration_all
+;
+
+--#SET :DROP FOREIGN KEY::FK93_wellbore_exploration_all
+ALTER TABLE "npd_no_spatial_10"."wellbore_exploration_all"
+DROP CONSTRAINT FK93_wellbore_exploration_all
+;
+
+--#SET :DROP FOREIGN KEY::FK94_wellbore_exploration_all
+ALTER TABLE "npd_no_spatial_10"."wellbore_exploration_all"
+DROP CONSTRAINT FK94_wellbore_exploration_all
+;
+
+--#SET :DROP FOREIGN KEY::FK95_wellbore_oil_sample
+ALTER TABLE "npd_no_spatial_10"."wellbore_oil_sample"
+DROP CONSTRAINT FK95_wellbore_oil_sample
+;
+
+--#SET :DROP FOREIGN KEY::FK96_wellbore_shallow_all
+ALTER TABLE "npd_no_spatial_10"."wellbore_shallow_all"
+DROP CONSTRAINT FK96_wellbore_shallow_all
+;
+
