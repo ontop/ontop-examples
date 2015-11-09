5ddd7529d21b6294d4c41f6054640fa1f75fc10f
Benjamin Cogrel <benjamin.cogrel@bcgl.fr>
2015-11-09 19:39:49 +0100

    Integration tables added.

---
+++ tours-tutoriel-2015/insert_others.sql
@@ -0,0 +1,14 @@
+INSERT INTO T_Name 
+(PId,Nombre) VALUES
+(1,'Anna'),
+(2,'Mike');
+
+INSERT INTO T_NSCLC 
+(Id,hosp,Stge) VALUES
+(1,'X', 'two'),
+(2,'Y', 'one');
+
+INSERT INTO T_SCLC 
+(key,hosp) VALUES
+(1,'XXX'),
+(2,'YYY');
