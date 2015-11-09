e3e7e2109be94ae5682eccf2b083fae32c54991a
Benjamin Cogrel <benjamin.cogrel@bcgl.fr>
2015-11-09 20:43:18 +0100

    Table slightly refactored.

--- tours-tutoriel-2015/insert_others.sql
+++ tours-tutoriel-2015/insert_others.sql
@@ -1,14 +1,14 @@
 INSERT INTO T_Name 
-(PId,Nombre) VALUES
+(pid,name) VALUES
 (1,'Anna'),
 (2,'Mike');
 
 INSERT INTO T_NSCLC 
-(Id,hosp,Stge) VALUES
+(pid,hosp,stage) VALUES
 (1,'X', 'two'),
 (2,'Y', 'one');
 
 INSERT INTO T_SCLC 
-(key,hosp) VALUES
-(1,'XXX'),
-(2,'YYY');
+(pid,hosp) VALUES
+(1,'Z'),
+(2,'X');
