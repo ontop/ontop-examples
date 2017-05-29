30ac2f1643cbfb2ccedc69755450e0540d018d9e
tir <davide.lanti@unibz.it>
2017-05-29 10:26:57 +0200

    3 atoms

---
+++ iswc-2017-cost/3-atoms/README.md
@@ -0,0 +1,4 @@
+# Content of This Folder
+
+- Ontology, Mappings, and SPARQL Query Files
+- SQL Folder: It contains the SQL (jucq or ucq) unfoldings.
---
+++ iswc-2017-cost/3-atoms/sql/jucq-unfoldings/jucq-43.sql
@@ -0,0 +1,33 @@
+SELECT *
+FROM
+(SELECT DISTINCT qview1."unique2" AS t0v0,  qview1."evenonepercent" AS t1v0,  qview1."stringu1" AS t1v1,  qview1."stringu2" AS t1v2,  qview2."evenonepercent" AS t2v0,  qview2."stringu1" AS t2v1,  qview2."stringu2"
+AS t2v2 FROM
+"public"."t1_1m" qview1,
+"public"."t2_1m" qview2
+WHERE
+((qview1."onepercent" >= 0) AND (qview1."onepercent" < 20)) AND
+qview1."evenonepercent" IS NOT NULL AND
+qview1."stringu2" IS NOT NULL AND
+qview1."stringu1" IS NOT NULL AND
+qview1."unique2" IS NOT NULL AND
+(qview1."unique2" = qview2."unique2") AND
+((qview2."onepercent" >= 5) AND (qview2."onepercent" < 25)) AND
+qview2."evenonepercent" IS NOT NULL AND
+qview2."stringu2" IS NOT NULL AND
+qview2."stringu1" IS NOT NULL
+) f_1, (SELECT DISTINCT qview1."unique2" AS t0v0,  qview2."evenonepercent" AS t3v0,  qview2."stringu1" AS t3v1,  qview2."stringu2"
+AS t3v2 FROM
+"public"."t1_1m" qview1,
+"public"."t3_1m" qview2
+WHERE
+((qview1."onepercent" >= 0) AND (qview1."onepercent" < 20)) AND
+qview1."evenonepercent" IS NOT NULL AND
+qview1."stringu2" IS NOT NULL AND
+qview1."stringu1" IS NOT NULL AND
+qview1."unique2" IS NOT NULL AND
+(qview1."unique2" = qview2."unique2") AND
+((qview2."onepercent" >= 5) AND (qview2."onepercent" < 25)) AND
+qview2."evenonepercent" IS NOT NULL AND
+qview2."stringu2" IS NOT NULL AND
+qview2."stringu1" IS NOT NULL
+) f_2 WHERE f_1.t0v0=f_2.t0v0
