3c9716e251e0de1db0c225532c1383310de7c97f
juliencorman <cormanjulien@gmail.com>
2017-10-08 17:55:49 +0200

    Cleaned executables

--- iswc-2017-mongodb/exec/morph/dblp_sample/readLogs.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-for FILE in $1/*.out
-do
-    FILENAME=$(basename $FILE)
-    TIME=$(cat $FILE | grep "Overall SPARQL query processing time" | awk '{print $13}' | sed -n "s/\([0-9]*\)ms./\1/p" )
-    echo $FILE "," $TIME
-done
-
--- iswc-2017-mongodb/exec/morph/dblp_sample/runAll.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-for FILE in ../student.queries/*.rq
-do
-    echo "$FILE"
-    ./runSparql.sh $FILE
-done
-
