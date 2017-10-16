6ee1cdaf21e6f46fb614931f4b957c2cb6c63f3d
juliencorman <cormanjulien@gmail.com>
2017-10-16 15:16:55 +0200

    Deleted iswc-2017-mongodb repository

--- iswc-2017-mongodb/data/dblp/queries/drill/publications/q01_author.sql
+++
@@ -1,7 +0,0 @@
-SELECT 'http://dblp.l3s.de/d2r/authors/' || `author`['_id'] AS x FROM (
-  SELECT
-    _id,
-    FLATTEN(`authors`) AS `author`
-  FROM mongo.`dblp_publications`.`publications`
-) t
-LIMIT 50000
